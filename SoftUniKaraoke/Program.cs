e17874c6cb338cbd00f80a19372597bf4c7f236e
slavCode <stomow@gmail.com>
2022-05-11 16:19:58 +0300

    Add SoftUni Karaoke

--- SoftUniKaraoke/Program.cs
+++ SoftUniKaraoke/Program.cs
@@ -1,8 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace SoftUniKaraoke
 {
@@ -10,6 +8,53 @@ namespace SoftUniKaraoke
     {
         static void Main(string[] args)
         {
+            var realParticipants = Console.ReadLine()
+                                         .Split(new string[] { ", " }, StringSplitOptions.RemoveEmptyEntries)
+                                         .ToList();
+            var songs = Console.ReadLine()
+                                        .Split(new string[] { ", " }, StringSplitOptions.RemoveEmptyEntries)
+                                        .ToList();
+
+            var resultedParticipants = new Dictionary<string, Dictionary<string, string>>();
+            string input = Console.ReadLine();
+            while (input != "dawn")
+            {
+                string[] parts = input.Split(new string[] { ", " }, StringSplitOptions.RemoveEmptyEntries);
+                string participant = parts[0];
+                string song = parts[1];
+                string awardName = parts[2];
+
+                if (realParticipants.Contains(participant) && songs.Contains(song))
+                {
+                    if (!resultedParticipants.ContainsKey(participant))
+                    {
+                        var award = new Dictionary<string, string>();
+                        award.Add(awardName, song);
+                        resultedParticipants.Add(participant, award);
+                    }
+                    else if (!resultedParticipants[participant].ContainsKey(awardName))
+                    {
+                        resultedParticipants[participant].Add(awardName, song);
+                    }
+                }
+                input = Console.ReadLine();
+            };
+
+            if (resultedParticipants.Count < 1)
+            {
+                Console.WriteLine("No awards");
+                return;
+            }
+
+            var sortedParticipants = resultedParticipants.OrderByDescending(a => a.Value.Count()).ThenBy(p => p.Key);
+            foreach (var participant in sortedParticipants)
+            {
+                Console.WriteLine($"{participant.Key}: {participant.Value.Count()} awards");
+                foreach (var award in participant.Value.OrderBy(a => a.Key))
+                {
+                    Console.WriteLine($"--{award.Key}");
+                }
+            }
         }
     }
 }
