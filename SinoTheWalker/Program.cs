24bb2aa1735b19afc39118eae09d05cae2ee7456
slavCode <stomow@gmail.com>
2022-05-11 15:08:17 +0300

    Add project files.

---
+++ SinoTheWalker/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Globalization;
+
+namespace SinoTheWalker
+{
+    internal class Program
+    {
+        static void Main()
+        {
+            DateTime leaveTime = DateTime.ParseExact(Console.ReadLine(), "HH:mm:ss", CultureInfo.InvariantCulture);
+            //1 day = 86400
+            int steps = int.Parse(Console.ReadLine()) % 86400;
+            int stepTime = int.Parse(Console.ReadLine()) % 86400;
+
+            double stepsInSec = (steps * stepTime);
+            DateTime arrivalTime = leaveTime.AddSeconds(stepsInSec);
+
+            Console.WriteLine($"Time Arrival: {arrivalTime:HH:mm:ss}");
+        }
+    }
+}
